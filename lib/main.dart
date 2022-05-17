62649350a53871c78294c060b0fa4a82610568e3
yasui-kohei <yasui.kohei.xj@alumni.tsukuba.ac.jp>
2022-05-17 23:42:18 +0900

    FirebaseのプロジェクトにWebアプリを追加
    安井康平

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:lt02_demo/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
